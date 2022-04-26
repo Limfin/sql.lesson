028cd66fbdf24669372ab076ddb06582676001e2
Limfin <victor010389@yandex.ru>
2022-04-26 11:03:38 +0400

    Добавление новой записи в таблицу. Добавление нескольких записей в цикле. Обновление существующей записи в таблице. Удаление записи из таблицы

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
 
 <body>
 	<h1>PHP - MySQL</h1>
-<?php
+	<?php
 	//подключение к базе данных
-	$mysql = new mysqli('localhost', 'root','','sql_less');
+	$mysql = new mysqli('localhost', 'root', '', 'sql_less');
 	$mysql->query("SET NAMES 'utf8'");
 
-	if($mysql->connect_error) {
+	if ($mysql->connect_error) {
 		echo 'Error Number: ' . $mysql->connect_errno . '<br>';
 		echo 'Error: ' . $mysql->connect_error;
 	} else {
@@ -23,16 +23,32 @@
 		// $mysql->query("DROP TABLE `example`");
 
 		//создание новой таблицы в базе данных
-		$mysql->query("CREATE TABLE `users` (
+		/* $mysql->query("CREATE TABLE `users` (
 			id INT(11) NOT NULL,
 			name VARCHAR(50) NOT NULL,
 			bio TEXT NOT NULL,
 			PRIMARY KEY(id)
-		)");
+		)"); */
+
+
+		//добавлений новой записи в таблицу
+		$mysql->query("INSERT INTO `users` (`name`, `bio`) VALUES('John', 'Full text')");
+
+		//добавление нескольких записей в цикле
+		for ($i=1; $i <= 5; $i++) { 
+			$name = "Bob #" . $i;
+			$mysql->query("INSERT INTO `users` (`name`, `bio`) VALUES('$name', 'Full text')");
+		}
+
+		//обновление существующей записи в таблице
+		$mysql->query("UPDATE `users` SET `bio` = 'New text' WHERE `id` = 2");
+
+		//удаление записи из таблицы
+		$mysql->query("DELETE FROM `users` WHERE `id` = 5 OR `id` = 4");
 	}
 
 	$mysql->close();
-?>
+	?>
 </body>
 
 </html>
